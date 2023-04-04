b910cea540eafe0d4f5a242e9106444320cd1e6a
SallenSan <sallenkon54@gmail.com>
2023-04-03 22:26:31 -0300

    update

--- App.java
+++ App.java
@@ -1,5 +1,7 @@
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -25,15 +27,19 @@ public class App {
 
 
         // exibição e manipulação de dados
-        for (Map<String, String> filme : listaDeFilmes){
-            System.out.println(filme.get("title"));
-            System.out.println(filme.get("image"));
-            System.out.println(filme.get("imDbRating"));
+        var geradora = new GeradoraDeFigurinhas();
+        for (Map<String,String> filme : listaDeFilmes){
+
+            String urlImagem = filme.get("image");
+            String titulo = filme.get("title");
+
+            InputStream inputStream = new URL(urlImagem).openStream();
+            String nomeArquivo = titulo + ".png";
+
+            geradora.cria(inputStream, nomeArquivo);
+
+            System.out.println(titulo);
             System.out.println();
-
         }
-        
-
     }
-
 }
--- GeradoraDeFigurinhas.java
+++ GeradoraDeFigurinhas.java
@@ -3,14 +3,18 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+
 
 public class GeradoraDeFigurinhas {
 
 
-    public void create () throws IOException {
+    public void cria(InputStream inputStream, String nomeArquivo) throws IOException {
 
         // leitura da imagem
-        BufferedImage imagemOriginal = ImageIO.read(new File("input/imagem.jpg"));
+       // InputStream inputStream = new URL("https://m.media-amazon.com/images/M/MV5BNDE3ODcxYzMtY2YzZC00NmNlLWJiNDMtZDViZWM2MzIxZDYwXkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_UX128_CR0,3,128,176_AL_.jpg").openStream();
+
+        BufferedImage imagemOriginal = ImageIO.read(inputStream);
 
 
         // crie nova imagem em memória com transparência e com novo tamanho
@@ -24,21 +28,24 @@ public class GeradoraDeFigurinhas {
         Graphics2D graphics = (Graphics2D) novaImagem.getGraphics();
         graphics.drawImage(imagemOriginal, 0, 0, null);
 
+        // configurar a fonte
+        var fonte = new Font(Font.SANS_SERIF, Font.BOLD, 64);
+        graphics.setColor(Color.YELLOW);
+        graphics.setFont(fonte);
+
 
         // escreva uma frase na nova imagem
+        graphics.drawString("Topzera", 100, novaAltura - 100);
 
 
         // escrever a nova imagem num arquivo
-        ImageIO.write(novaImagem, "png", new File("saida/figurinhas.png"));
+        ImageIO.write(novaImagem, "png", new File(nomeArquivo));
 
 
 
     }
 
-         public static void main(String[] args) throws IOException {
-            var geradora = new GeradoraDeFigurinhas();
-            geradora.create();
-        }
+
 
 
 }
